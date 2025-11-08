0e3a57a29f481ab2d65df6bab98a1c31bcf678f2
juliabaptiste36-ctrl <juliabaptiste36@gmail.com>
2025-11-07 20:23:39 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function myFunction() {var a = 4; document.getElementById("demo").innerHTML = a*a;}
+function validate(e) {e.preventDefault();}
+
+const email = document.getElementsById('email').value;
+const pass = document.getElementsById('password').value;
+const age = document.getElementsById('age').value;
+const msgBox = document.getElementsById('message').value;
+
+let message 
