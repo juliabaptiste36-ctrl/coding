d08a5c234858228cd5423c847a2fb5e2060a6f22
juliabaptiste36-ctrl <juliabaptiste36@gmail.com>
2025-12-10 20:36:24 -0400

    Add files via upload

---
+++ doublecheck.py
@@ -0,0 +1,13 @@
+num = int(input("Enter Number to check:"))
+
+if num>50:
+    print("Number is greater tgan 50 :")
+    if num%2==0:
+        print("And it is even too")
+    else:
+        print("And it is odd")
+
+
+else:
+
+    print("Number is less than 50")
